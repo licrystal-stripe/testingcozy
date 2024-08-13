df4ee4ed1a1f363d898e6c1c934994161f48e576
Crystal Li <licrystal@stripe.com>
2024-08-12 23:41:42 -0500

    changing the return url

--- components/CustomCheckoutFormUpdated.jsx
+++ components/CustomCheckoutFormUpdated.jsx
@@ -100,7 +100,7 @@ useEffect(() => {
     const { error } = await stripe.confirmPayment({
       elements,
       confirmParams: {
-        return_url: "http://localhost:3000/?payment_status=success",
+        return_url: `${req.headers.origin}/?payment_status=success`,
         //confirmation_token: confirmationToken
       },
     });
